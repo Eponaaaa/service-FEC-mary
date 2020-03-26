34bcd17e24a27a6b8a3cc1d5d57cf0f782a62693
mhchow <marychow9@gmail.com>
2020-03-26 12:33:06 -0700

    refactored to use window.productId

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MoreFrom from './components/MoreFromContainer.jsx';
+import '../public/styles.css';
 
-
+const { productId } = window;
 ReactDOM.render(
-  <MoreFrom />,
+  <MoreFrom productId={productId} />,
   document.getElementById('app4')
 );
